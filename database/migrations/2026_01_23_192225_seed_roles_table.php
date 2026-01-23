131596e10e9afc5e7753de9d1b5a81cabdbbfdf4
hugo bellin <hugoblln@NicoVictus.localdomain>
2026-01-23 20:26:58 +0100

    create roles

---
+++ database/migrations/2026_01_23_192225_seed_roles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $roles = ['Admin', 'Author', 'viewer'];
+
+        foreach($roles as $role)
+        {
+            Role::create(['name' => $role]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Role::whereIn('name', ['admin','author','user'])->delete();
+    }
+};
